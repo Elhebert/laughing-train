2cfb702efe0b3c495b3827fb7d639d31f8b3bb7c
Faik Malik <faik.malik@trivago.com>
2020-10-23 12:27:39 +0200

    feat(server/utils): added utils

---
+++ server/utils/bindandvalid.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"net/http"
+
+	"github.com/astaxie/beego/validation"
+	"github.com/gin-gonic/gin"
+)
+
+// BindAndValid binds and validates data
+func BindAndValid(c *gin.Context, form interface{}) (int, int) {
+	err := c.Bind(form)
+	if err != nil {
+		return http.StatusBadRequest, 400
+	}
+
+	valid := validation.Validation{}
+	check, err := valid.Valid(form)
+	if err != nil {
+		return http.StatusInternalServerError, 500
+	}
+	if !check {
+		return http.StatusBadRequest, 400
+	}
+
+	return http.StatusOK, 200
+}
